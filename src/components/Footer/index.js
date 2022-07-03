f53bab7f191bc137e6ac94dbd5fdd1354d806530
VSartz <elsartz@gmail.com>
2022-07-03 13:05:02 -0400

    trying to import icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
-// import coverImage from "../../assets/cover/cover-image.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faArrowCircleUp } from '@fortawesome/free-solid-svg-icons';
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Footer() {
 
   return (
     <footer className="App">
-    Copyright by <a href="http://github.com/elsartz">Vardis Sartzetakis</a> 2022
+      <FontAwesomeIcon icon={solid('user-secret')} />
+       Copyright by <a href="http://github.com/elsartz">Vardis Sartzetakis </a> 2022
+    {/* <FontAwesomeIcon icon={brands('twitter')} /> */}
+    <FontAwesomeIcon icon={faThumbsUp} />
   </footer>
   )
 }
