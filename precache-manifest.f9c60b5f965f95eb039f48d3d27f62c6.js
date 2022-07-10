6fe1755157ac33204c04c2cd5641be029c69fff8
VSartz <elsartz@gmail.com>
2022-07-10 09:35:07 -0400

    Updates

---
+++ precache-manifest.f9c60b5f965f95eb039f48d3d27f62c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d204a73f201deead011050aeded3d53",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9ee8a1df7b372e86eeca",
+    "url": "/react-portfolio/static/css/main.37e9b685.chunk.css"
+  },
+  {
+    "revision": "cfa72adda61d31077d64",
+    "url": "/react-portfolio/static/js/2.8bfe1679.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/react-portfolio/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "9ee8a1df7b372e86eeca",
+    "url": "/react-portfolio/static/js/main.3fd8bfef.chunk.js"
+  },
+  {
+    "revision": "52a6c3a7c7c04ea0b0e4",
+    "url": "/react-portfolio/static/js/runtime~main.7e8c8cb4.js"
+  }
+]);
