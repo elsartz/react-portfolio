4270a20375dca8172184cdf96f623b7dc6949630
VSartz <elsartz@gmail.com>
2022-08-08 14:49:00 -0400

    Updates

---
+++ precache-manifest.0629dd960680600c3b89b86ca6c063df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6819d38a17a44b53d9d",
+    "url": "/react-portfolio/static/css/main.a02aa901.chunk.css"
+  },
+  {
+    "revision": "e6819d38a17a44b53d9d",
+    "url": "/react-portfolio/static/js/main.e6819d38.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/react-portfolio/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "b01299218bedf6e92cbd",
+    "url": "/react-portfolio/static/js/2.b0129921.chunk.js"
+  },
+  {
+    "revision": "fb8d19f71ffd14d5e37a",
+    "url": "/react-portfolio/static/js/runtime~main.fb8d19f7.js"
+  },
+  {
+    "revision": "39d0570dabe426b244216d6d0df1d986",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.4460712258289a49158d0aaf2455930b.js"
+  "/react-portfolio/precache-manifest.0629dd960680600c3b89b86ca6c063df.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
